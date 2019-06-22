d8cdbf5263883a0d1d3c8557dc687d3b12978f11
ErikBoesen <me@erikboesen.com>
2019-06-21 20:54:36 -0400

    Expand example

--- example.py
+++ example.py
@@ -5,6 +5,14 @@ import os
 api = yalecourses.YaleCourses(os.environ['YALE_API_KEY'])
 
 courses = api.courses('CPSC')
-print('There are %d courses in this subject area' % len(courses))
+print('There are %d courses in this subject area:' % len(courses))
 for course in courses:
-    print(f"{course.number}: {course.name}, {course.classpath}")
+    print(f"- {course.number}: {course.name}")
+
+course = api.course('CPSC201')
+print(course.code + ':')
+print(course.raw_description)
+
+# If you want more specificity
+course = api.course(subject='CPSC', number=201, year=2015, term=1)
+print(f'The {course.year} version of {course.code} was taught primarily by {course.instructors[0]}.')
