aa58ad6d113176d5650098f690bead5634063ba9
ErikBoesen <me@erikboesen.com>
2020-02-06 17:22:58 -0500

    Bump to 0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='yalecourses',
-      version='0.1.4',
+      version='0.1.5',
       description='Library for fetching data from the Yale Courses API.',
       url='https://github.com/ErikBoesen/yalecourses',
       author='Erik Boesen',
